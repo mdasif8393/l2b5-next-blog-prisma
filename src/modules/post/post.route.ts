4673c9eba2a4a94dd88136c39d9eed20861db11f
mdasif8393 <md.asif.8393@gmail.com>
2025-09-18 13:26:17 +0600

    complete

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -3,6 +3,8 @@ import { PostController } from "./post.controller";
 
 const router = express.Router();
 
+router.get("/stats", PostController.getBlogStat);
+
 router.post("/", PostController.createPost);
 
 router.get("/", PostController.getAllPosts);
